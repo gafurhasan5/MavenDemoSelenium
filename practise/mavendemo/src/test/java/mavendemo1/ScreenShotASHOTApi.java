5fd0075deb217396fafbdd92b060a806ae6d2aca
gafurhasan5 <gafurhasan5@gmail.com>
2025-11-05 15:23:55 +0530

    AShot Api for Full Schreenshot

---
+++ practise/mavendemo/src/test/java/mavendemo1/ScreenShotASHOTApi.java
@@ -0,0 +1,27 @@
+package mavendemo1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import ru.yandex.qatools.ashot.AShot;
+import ru.yandex.qatools.ashot.Screenshot;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+
+public class ScreenShotASHOTApi {
+    public static void main(String[] args) throws IOException, AWTException {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://omayo.blogspot.com/");
+        driver.findElement(By.id("alert1")).click();
+        Screenshot screenshot = new AShot().shootingStrategy(shootingStrategies.viewportPasting(1000)).takeScreenshot(driver);
+        ImageIO.write(screenshot.getImage(), "PNG", new File(System.getProperty("user.dir") + "/Screenshot/fullimage.png"));
+
+    }
+
+
+    }
+}
